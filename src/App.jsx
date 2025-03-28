1601ac3e4ac150c56f88bbdda3aeda0a05f5686b
Yuliana Mallorga <yulianamallorga@gmail.com>
2025-03-28 00:13:49 -0300

    Format the code and create the pointer class to add styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,70 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 const FollowMouse = () => {
-  const [enabled, setEnabled] = useState(false)
-  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   // pointer move
   useEffect(() => {
-    console.log('effect ', { enabled })
-
     const handleMove = (event) => {
-      const { clientX, clientY } = event
-      setPosition({ x: clientX, y: clientY })
-    }
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
 
     if (enabled) {
-      window.addEventListener('pointermove', handleMove)
+      window.addEventListener("pointermove", handleMove);
     }
-
-    return () => { // cleanup method
-      window.removeEventListener('pointermove', handleMove)
-    }
-  }, [enabled])
-
-  // [] -> solo se ejecuta una vez cuando se monta el componente
-  // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
-  // undefined -> se ejecuta cada vez que se renderiza el componente
-
-  // change body className
-  useEffect(() => {
-    document.body.classList.toggle('no-cursor', enabled)
 
     return () => {
-      document.body.classList.remove('no-cursor')
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+
+  useEffect(() => {
+    const pointer = document.querySelector(".pointer");
+
+    if (pointer) {
+      pointer.style.display = enabled ? "block" : "none";
     }
-  }, [enabled])
+
+    document.body.classList.toggle("no-cursor", enabled);
+
+    return () => {
+      document.body.classList.remove("no-cursor");
+    };
+  }, [enabled]);
 
   return (
     <>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        border: '2px solid #fff',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -25,
-        top: -25,
-        width: 50,
-        height: 50,
-        transform: `translate(${position.x}px, ${position.y}px)`
-      }}
-      />
+      <div
+        className="pointer"
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          border: "2px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      ></div>
       <button onClick={() => setEnabled(!enabled)}>
-        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
       </button>
     </>
-  )
-}
+  );
+};
 
-function App () {
+function App() {
   return (
     <main>
       <FollowMouse />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
